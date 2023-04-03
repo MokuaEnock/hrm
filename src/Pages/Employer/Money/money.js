63eb9961c7c3a5b08004c57491c8e4407baa2e2a
Enock <enock.mokua@student.moringaschool.com>
2023-04-03 15:38:38 +0300

    add functional code to eployer money

--- src/Pages/Employer/Money/money.js
+++ src/Pages/Employer/Money/money.js
@@ -8,6 +8,26 @@ export default function EmployerMoney() {
       <EmployerNav />
       <section id="employer-money-cont">
         <EmployerHead message={"Manage your finances today."} />
+        <section id="client-payment-cont">
+          <div id="client-payment-cont1">
+            <h3>Release Employees Salaries</h3>
+            {data.map((department) => (
+              <Link
+                className="payment-list"
+                key={department.department_id}
+                to={`/client/signup/${department.department_id}`}
+              >
+                <span className="payment-list-head">
+                  {department.department_id}
+                </span>
+                <span className="payment-list-body">
+                  <p>{department.department_name}</p>
+                </span>
+              </Link>
+            ))}
+          </div>
+          <aside id="client-payment-cont2"></aside>
+        </section>
       </section>
     </section>
   );
