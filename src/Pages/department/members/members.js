9bbac58a0f301588dd78b373e933db3cc99bf2f4
Enock <enock.mokua@student.moringaschool.com>
2023-04-04 04:07:59 +0300

    create employees data and edit the table header

--- src/Pages/department/members/members.js
+++ src/Pages/department/members/members.js
@@ -4,9 +4,116 @@ import "./embers.css";
 import Table from "./table";
 
 const users = [
-  { id: 1, name: "John Doe", email: "john.doe@example.com" },
-  { id: 2, name: "Jane Smith", email: "jane.smith@example.com" },
-  { id: 3, name: "Bob Johnson", email: "bob.johnson@example.com" },
+  {
+    id: 1,
+    name: "John Doe",
+    email: "john.doe@example.com",
+    employeeNumber: "E001",
+    age: 32,
+    gender: "Male",
+    employeeRole: "Software Engineer",
+    daysPresent: 20,
+    daysAbsent: 5,
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+    employeeNumber: "E002",
+    age: 28,
+    gender: "Female",
+    employeeRole: "Designer",
+    daysPresent: 18,
+    daysAbsent: 7,
+  },
+  {
+    id: 3,
+    name: "Bob Johnson",
+    email: "bob.johnson@example.com",
+    employeeNumber: "E003",
+    age: 45,
+    gender: "Male",
+    employeeRole: "Project Manager",
+    daysPresent: 22,
+    daysAbsent: 3,
+  },
+  {
+    id: 4,
+    name: "Alice Lee",
+    email: "alice.lee@example.com",
+    employeeNumber: "E004",
+    age: 27,
+    gender: "Female",
+    employeeRole: "Software Engineer",
+    daysPresent: 21,
+    daysAbsent: 4,
+  },
+  {
+    id: 5,
+    name: "David Brown",
+    email: "david.brown@example.com",
+    employeeNumber: "E005",
+    age: 36,
+    gender: "Male",
+    employeeRole: "Data Analyst",
+    daysPresent: 19,
+    daysAbsent: 6,
+  },
+  {
+    id: 6,
+    name: "Mary Taylor",
+    email: "mary.taylor@example.com",
+    employeeNumber: "E006",
+    age: 31,
+    gender: "Female",
+    employeeRole: "UI/UX Designer",
+    daysPresent: 20,
+    daysAbsent: 5,
+  },
+  {
+    id: 7,
+    name: "Peter Chen",
+    email: "peter.chen@example.com",
+    employeeNumber: "E007",
+    age: 29,
+    gender: "Male",
+    employeeRole: "Software Engineer",
+    daysPresent: 18,
+    daysAbsent: 7,
+  },
+  {
+    id: 8,
+    name: "Linda Wang",
+    email: "linda.wang@example.com",
+    employeeNumber: "E008",
+    age: 33,
+    gender: "Female",
+    employeeRole: "Data Scientist",
+    daysPresent: 22,
+    daysAbsent: 3,
+  },
+  {
+    id: 9,
+    name: "Tom Smith",
+    email: "tom.smith@example.com",
+    employeeNumber: "E009",
+    age: 38,
+    gender: "Male",
+    employeeRole: "Software Engineer",
+    daysPresent: 21,
+    daysAbsent: 4,
+  },
+  {
+    id: 10,
+    name: "Emily Davis",
+    email: "emily.davis@example.com",
+    employeeNumber: "E010",
+    age: 26,
+    gender: "Female",
+    employeeRole: "Product Manager",
+    daysPresent: 19,
+    daysAbsent: 6,
+  },
 ];
 
 const columns = ["name", "email"];
@@ -28,6 +135,19 @@ export default function DepartmentMembers() {
             <button type="submit">Search</button>
           </span>
         </form>
+        <h3>Statistics this month</h3>
+        <section id="department-employees">
+          <div id="department-employees-header">
+            <span>Employee No.</span>
+            <span>Name</span>
+            <span>Email</span>
+            <span>Age</span>
+            <span>Gender</span>
+            <span>Role</span>
+            <span>Days Present</span>
+            <span>Days Absent</span>
+          </div>
+        </section>
 
         {/* <Table data={users} columns={columns} /> */}
       </section>
