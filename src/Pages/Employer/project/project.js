88d30309b1d5034bded321436514fc7c2df2d6d5
Enock <enock.mokua@student.moringaschool.com>
2023-04-06 00:36:38 +0300

    create general structure for employer projects

--- src/Pages/Employer/project/project.js
+++ src/Pages/Employer/project/project.js
@@ -4,10 +4,25 @@ import "./project.css";
 
 export default function EmployerProject() {
   return (
-    <section id="employer-container">
+    <section className="employer-container">
       <EmployerNav />
       <section id="employer-project">
-        <EmployerHead />
+        <EmployerHead message={"Manage your projects"} />
+        <section id="employer-project-cont">
+          <div id="employer-projects-main">
+            <h3>Your current projects</h3>
+            <ol id="employer-projects-list">
+              <li className="employer-projects-items"></li>
+            </ol>
+          </div>
+          <aside id="employer-projects-aside">
+            <h3>Project Vanilla</h3>
+            <span id="project-buttons">
+              <button id="project-update-button">Update</button>
+              <button id="project-delete-button">Delete</button>
+            </span>
+          </aside>
+        </section>
       </section>
     </section>
   );
