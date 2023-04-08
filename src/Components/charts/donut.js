6dcdb49f73b91527c7ed12775c0c23d012d3d512
Enock <enock.mokua@student.moringaschool.com>
2023-04-08 17:06:38 +0300

    chnage colors of the donut plot

--- src/Components/charts/donut.js
+++ src/Components/charts/donut.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Doughnut } from "react-chartjs-2";
-import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement } from "chart.js";
+import { Chart as ChartJS, Title, Tooltip, ArcElement } from "chart.js";
 
-ChartJS.register(ArcElement, Title, Tooltip, Legend);
+ChartJS.register(Title, Tooltip, ArcElement);
 
 const data = {
   labels: ["Red", "Green", "Yellow"],
@@ -10,12 +10,31 @@ const data = {
     {
       label: "My First Dataset",
       data: [300, 50, 100],
-      backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+      backgroundColor: [
+        "rgba(0, 99, 132, 0.8)",
+        "rgba(54, 162, 235, 0.8)",
+        "rgba(99, 132, 0, 0.8)",
+      ],
       hoverOffset: 4,
     },
   ],
 };
 
+const options = {
+  responsive: true,
+  plugins: {
+    tooltip: {
+      enabled: true,
+      backgroundColor: "rgba(255,255,255,0.8)",
+      bodyColor: "rgba(0,0,0,0.8)",
+    },
+  },
+};
+
 export default function DonutGraph() {
-  return <Doughnut data={data} />;
+  return (
+    <div style={{ height: "100%", width: "100%" }}>
+      <Doughnut data={data} options={options} />
+    </div>
+  );
 }
