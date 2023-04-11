54a1981e2efa6fa1a85ab8d82be0aea519b87bb5
Enock <enock.mokua@student.moringaschool.com>
2023-04-11 12:40:37 +0300

    set up stylign for lists of employees

--- src/Pages/Employer/Money/dept.js
+++ src/Pages/Employer/Money/dept.js
@@ -54,59 +54,63 @@ export default function EmployerDept() {
       <section id="employer-money-cont">
         <EmployerHead message={"Manage your finances today."} />
         <section id="client-payment-cont">
-            <div id="client-payment-cont1">
-              <h5>Engineering department</h5>
-              <div id="client-payment-cont-1-1">
-                <span className="client-payment-cont-1-1"></span>
-                <span className="client-payment-cont-1-1"></span>
-                <span className="client-payment-cont-1-1"></span>
-                <span className="client-payment-cont-1-1"></span>
-              </div>
-
-              <form id="client-payment-cont-1-3" onSubmit={handleSetPay}>
-                <h5>Set pay period</h5>
-                <span>
-                  <label>
-                    <p>Start date</p>
-                    <input
-                      type="date"
-                      value={startDate}
-                      onChange={handleStartDateChange}
-                    />
-                  </label>
-
-                  <label>
-                    <p>End date</p>
-                    <input
-                      type="date"
-                      value={endDate}
-                      onChange={handleEndDateChange}
-                    />
-                  </label>
-                </span>
-
-                <button type="submit">Set pay period</button>
-              </form>
-
-              <h5>Top Earners</h5>
-              <div id="client-payment-cont-1-2-head">
-                <span>Name</span>
-                <span>Days Present</span>
-                <span>Rating</span>
-                <span>Gross Pay</span>
-              </div>
-              <ol id="client-payment-cont-1-2">
-                <li className="client-payment-cont-1-2"></li>
-              </ol>
+          <div id="client-payment-cont1">
+            <h5>Engineering department</h5>
+            <div id="client-payment-cont-1-1">
+              <span className="client-payment-cont-1-1"></span>
+              <span className="client-payment-cont-1-1"></span>
+              <span className="client-payment-cont-1-1"></span>
+              <span className="client-payment-cont-1-1"></span>
             </div>
 
-            <aside id="client-payment-cont2">
-              <h4>Summary for this week</h4>
-              <button id="client-payment-button">Release Salary</button>
-            </aside>
+            <form id="client-payment-cont-1-3" onSubmit={handleSetPay}>
+              <h5>Set pay period</h5>
+              <span>
+                <label>
+                  <p>Start date</p>
+                  <input
+                    type="date"
+                    value={startDate}
+                    onChange={handleStartDateChange}
+                  />
+                </label>
+
+                <label>
+                  <p>End date</p>
+                  <input
+                    type="date"
+                    value={endDate}
+                    onChange={handleEndDateChange}
+                  />
+                </label>
+              </span>
+
+              <button type="submit">Set pay period</button>
+            </form>
+
+            <h5>Top Earners</h5>
+            <div id="client-payment-cont-1-2-head">
+              <span>Name</span>
+              <span>Days Present</span>
+              <span>Rating</span>
+              <span>Gross Pay</span>
+            </div>
+            <ol id="client-payment-cont-1-2">
+              <li className="client-payment-cont-1-2">
+                <span>Enock Mokua</span>
+                <span>21</span>
+                <span>8.5</span>
+                <span>10,200</span>
+              </li>
+            </ol>
+          </div>
+
+          <aside id="client-payment-cont2">
+            <h4>Summary for this week</h4>
+            <button id="client-payment-button">Release Salary</button>
+          </aside>
         </section>
       </section>
     </section>
   );
 }
-
