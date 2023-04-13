9ef41de1cd3ee62e1943186acd2e8e1f073d71e1
Enock <enock.mokua@student.moringaschool.com>
2023-04-13 23:41:33 +0300

    map the project data

--- src/Pages/Employer/project/project.js
+++ src/Pages/Employer/project/project.js
@@ -5,60 +5,48 @@ import "./project.css";
 let data = {
   projects: [
     {
+      id: 1,
       name: "Social Media Campaign",
+      department: "Marketing",
       progress: 75,
       start_date: "2023-01-01",
       end_date: "2023-03-31",
       budget: 100000,
       expenses: 75000,
       status: "In Progress",
-      departments: [
-        {
-          name: "Marketing",
-        },
-      ],
     },
     {
+      id: 2,
       name: "Market Research",
+      department: "Marketing",
       progress: 100,
       start_date: "2022-10-01",
       end_date: "2022-12-31",
       budget: 50000,
       expenses: 50000,
       status: "Completed",
-      departments: [
-        {
-          name: "Marketing",
-        },
-      ],
     },
     {
+      id: 3,
       name: "Website Redesign",
+      department: "IT",
       progress: 50,
       start_date: "2023-01-01",
       end_date: "2023-06-30",
       budget: 150000,
       expenses: 75000,
       status: "In Progress",
-      departments: [
-        {
-          name: "IT",
-        },
-      ],
     },
     {
+      id: 4,
       name: "Software Upgrade",
+      department: "IT",
       progress: 0,
       start_date: "2023-04-01",
       end_date: "2023-09-30",
       budget: 100000,
       expenses: 0,
       status: "Not Started",
-      departments: [
-        {
-          name: "IT",
-        },
-      ],
     },
   ],
 };
@@ -73,11 +61,19 @@ export default function EmployerProject() {
           <div id="employer-projects-main">
             <h3>Your current projects</h3>
             <ol id="employer-projects-list">
-              <li className="employer-projects-items">
-                <span className="employer-projects-items-head"></span>
-              </li>
+              {data.projects.map((project) => (
+                <li className="employer-projects-items" key={project.id}>
+                  <span className="employer-projects-items-head">
+                    {project.id}
+                  </span>
+                  <span className="employer-projects-items-body">
+                    {project.name}
+                  </span>
+                </li>
+              ))}
             </ol>
           </div>
+
           <aside id="employer-projects-aside">
             <h3>Project Vanilla</h3>
             <div id="employer-projects-aside-info">
