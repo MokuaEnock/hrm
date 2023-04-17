c4f5a4476923de3cf3a16e793c7472ef5001852c
Enock <enock.mokua@student.moringaschool.com>
2023-04-18 00:48:20 +0300

    fetch data for payslips

--- src/Pages/Employer/Money/dept.js
+++ src/Pages/Employer/Money/dept.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-lone-blocks */
 import "./dept.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import * as XLSX from "xlsx";
 import jsPDF from "jspdf";
@@ -16,6 +16,15 @@ export default function EmployerDept() {
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
   let [data, setData] = useState();
+  let [payslip, setPayslip] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:3000/payslip/1")
+      .then((res) => res.json())
+      .then((data) => {
+        setPayslip(data.payslips);
+      });
+  }, []);
 
   function handleSetPay(e) {
     e.preventDefault();
@@ -100,165 +109,183 @@ export default function EmployerDept() {
     }
   };
 
+  console.log(payslip);
+
   function handleSlips() {
-    let sample = [
-      {
-        basic_salary: 800,
-        gross_salary: 11600,
-        net_salary: 9900,
-        nssf_deduction: "200.0",
-        paye: "1100.0",
-        nhif: "400.0",
-        sacco: "1000.0",
-        week_dates: [
-          "2023-04-02",
-          "2023-04-03",
-          "2023-04-04",
-          "2023-04-05",
-          "2023-04-06",
-          "2023-04-07",
-          "2023-04-08",
-          "2023-04-09",
-          "2023-04-10",
-          "2023-04-11",
-          "2023-04-12",
-          "2023-04-13",
-          "2023-04-14",
-          "2023-04-15",
-        ],
-        week_pay: [
-          0,
-          "800.0",
-          "800.0",
-          "800.0",
-          "800.0",
-          "800.0",
-          "1600.0",
-          0,
-          "800.0",
-          "800.0",
-          "800.0",
-          "800.0",
-          0,
-          "1600.0",
-        ],
-        week_one: [
-          "2023-04-02",
-          "2023-04-03",
-          "2023-04-04",
-          "2023-04-05",
-          "2023-04-06",
-          "2023-04-07",
-          "2023-04-08",
-        ],
-        week_two: [
-          "2023-04-09",
-          "2023-04-10",
-          "2023-04-11",
-          "2023-04-12",
-          "2023-04-13",
-          "2023-04-14",
-          "2023-04-15",
-        ],
-      },
-    ];
+    // let sample = [
+    //   {
+    //     basic_salary: 800,
+    //     gross_salary: 11600,
+    //     net_salary: 9900,
+    //     nssf_deduction: "200.0",
+    //     paye: "1100.0",
+    //     nhif: "400.0",
+    //     sacco: "1000.0",
+    //     week_dates: [
+    //       "2023-04-02",
+    //       "2023-04-03",
+    //       "2023-04-04",
+    //       "2023-04-05",
+    //       "2023-04-06",
+    //       "2023-04-07",
+    //       "2023-04-08",
+    //       "2023-04-09",
+    //       "2023-04-10",
+    //       "2023-04-11",
+    //       "2023-04-12",
+    //       "2023-04-13",
+    //       "2023-04-14",
+    //       "2023-04-15",
+    //     ],
+    //     week_pay: [
+    //       0,
+    //       "800.0",
+    //       "800.0",
+    //       "800.0",
+    //       "800.0",
+    //       "800.0",
+    //       "1600.0",
+    //       0,
+    //       "800.0",
+    //       "800.0",
+    //       "800.0",
+    //       "800.0",
+    //       0,
+    //       "1600.0",
+    //     ],
+    //     week_one: [
+    //       "2023-04-02",
+    //       "2023-04-03",
+    //       "2023-04-04",
+    //       "2023-04-05",
+    //       "2023-04-06",
+    //       "2023-04-07",
+    //       "2023-04-08",
+    //     ],
+    //     week_two: [
+    //       "2023-04-09",
+    //       "2023-04-10",
+    //       "2023-04-11",
+    //       "2023-04-12",
+    //       "2023-04-13",
+    //       "2023-04-14",
+    //       "2023-04-15",
+    //     ],
+    //   },
+    // ];
 
-    let week_pay = sample[0].week_pay;
-    let half = Math.ceil(week_pay.length / 2);
-    let week_one_pay = week_pay.slice(0, half);
-    let week_two_pay = week_pay.slice(-half);
+    // let week_pay = sample[0].week_pay;
+    // let half = Math.ceil(week_pay.length / 2);
+    // let week_one_pay = week_pay.slice(0, half);
+    // let week_two_pay = week_pay.slice(-half);
 
-    let bodies1 = [sample[0].basic_salary, ...week_one_pay, ...week_two_pay];
-    let bodies2 = [
-      sample[0].gross_salary,
-      sample[0].nssf_deduction,
-      sample[0].nhif,
-      sample[0].paye,
-      sample[0].sacco,
-      sample[0].net_salary,
-    ];
+    // let bodies1 = [sample[0].basic_salary, ...week_one_pay, ...week_two_pay];
+    // let bodies2 = [
+    //   sample[0].gross_salary,
+    //   sample[0].nssf_deduction,
+    //   sample[0].nhif,
+    //   sample[0].paye,
+    //   sample[0].sacco,
+    //   sample[0].net_salary,
+    // ];
 
-    let headers2 = ["Gross Pay", "NSSF", "NHF", "PAYE", "SACCO", "Net Pay"];
+    // let headers2 = ["Gross Pay", "NSSF", "NHF", "PAYE", "SACCO", "Net Pay"];
 
-    console.log(bodies1, bodies2);
+    // console.log(bodies1, bodies2);
     // const zip = new JSZip();
     // const promises = [];
 
-    // for (const data of payslipData) {
-    //   const doc = new jsPDF("landscape");
-    //   doc.setFontSize(10);
+    for (const data of payslip) {
+      // const doc = new jsPDF("landscape");
+      // doc.setFontSize(10);
 
-    //   let headers = [
-    //     "Basic Pay",
-    //     ...data.week_one,
-    //     "Extra Pay 1",
-    //     ...data.week_two,
-    //   ];
-    //   let headers2 = ["Gross Pay", "NSSF", "NHF", "PAYE", "SACCO", "Net Pay"];
+      let headers = [
+        "Basic Pay",
+        ...data.week_one,
+        "Extra Pay 1",
+        ...data.week_two,
+      ];
+      let headers2 = ["Gross Pay", "NSSF", "NHF", "PAYE", "SACCO", "Net Pay"];
 
-    //   // add logo at top right corner
-    //   doc.addImage(logo, "PNG", 220, -15, 70, 70);
+      let week_pay = data.week_pay;
+      let half = Math.ceil(week_pay.length / 2);
+      let week_one_pay = week_pay.slice(0, half);
+      let week_two_pay = week_pay.slice(-half);
 
-    //   // employer heading
-    //   doc.text(
-    //     "Transatal consulting limited P.o Box 12-40200 Nairobi PaySlip week 45-46",
-    //     5,
-    //     10
-    //   );
+      let bodies1 = [data.basic_salary, ...week_one_pay, ...week_two_pay];
+      let bodies2 = [
+        data.gross_salary,
+        data.nssf_deduction,
+        data.nhif,
+        data.paye,
+        data.sacco,
+        data.net_salary,
+      ];
 
-    //   doc.text(`Pay No: ${data.pay_no}`, 10, 17);
-    //   doc.text(data.name, 10, 24);
+      console.log(bodies1, bodies2, headers, headers2);
+      // // add logo at top right corner
+      // doc.addImage(logo, "PNG", 220, -15, 70, 70);
 
-    //   // add employee data table
-    //   doc.autoTable({
-    //     startY: 30,
-    //     head: [headers],
-    //     body: [
-    //       [
-    //         data.name,
-    //         data.bank_account,
-    //         data.bank_code,
-    //         data.branch_code,
-    //         data.bank_name,
-    //         data.amount,
-    //       ],
-    //     ],
-    //     theme: "grid",
-    //     styles: {
-    //       textColor: [0, 0, 0],
-    //       fillColor: [255, 255, 255],
-    //       cellPadding: 2,
-    //     },
-    //   });
+      // // employer heading
+      // doc.text(
+      //   "Transatal consulting limited P.o Box 12-40200 Nairobi PaySlip week 45-46",
+      //   5,
+      //   10
+      // );
 
-    //   // add employee data table 2
-    //   doc.autoTable({
-    //     startY: 70,
-    //     head: [headers2],
-    //     body: [
-    //       [
-    //         data.name,
-    //         data.bank_account,
-    //         data.bank_code,
-    //         data.branch_code,
-    //         data.bank_name,
-    //         data.amount,
-    //       ],
-    //     ],
-    //     theme: "grid",
-    //     styles: {
-    //       textColor: [0, 0, 0],
-    //       fillColor: [255, 255, 255],
-    //       cellPadding: 2,
-    //     },
-    //     headStyles: { fillColor: [255, 255, 255], textColor: [0, 0, 0] },
-    //     tableLineWidth: 0,
-    //   });
+      // doc.text(`Pay No: ${data.pay_no}`, 10, 17);
+      // doc.text(data.name, 10, 24);
 
-    //   const pdfData = doc.output("arraybuffer");
-    //   zip.file(`${data.name}.pdf`, pdfData);
-    //   promises.push(pdfData);
-    // }
+      // // add employee data table
+      // doc.autoTable({
+      //   startY: 30,
+      //   head: [headers],
+      //   body: [
+      //     [
+      //       data.name,
+      //       data.bank_account,
+      //       data.bank_code,
+      //       data.branch_code,
+      //       data.bank_name,
+      //       data.amount,
+      //     ],
+      //   ],
+      //   theme: "grid",
+      //   styles: {
+      //     textColor: [0, 0, 0],
+      //     fillColor: [255, 255, 255],
+      //     cellPadding: 2,
+      //   },
+      // });
+
+      // // add employee data table 2
+      // doc.autoTable({
+      //   startY: 70,
+      //   head: [headers2],
+      //   body: [
+      //     [
+      //       data.name,
+      //       data.bank_account,
+      //       data.bank_code,
+      //       data.branch_code,
+      //       data.bank_name,
+      //       data.amount,
+      //     ],
+      //   ],
+      //   theme: "grid",
+      //   styles: {
+      //     textColor: [0, 0, 0],
+      //     fillColor: [255, 255, 255],
+      //     cellPadding: 2,
+      //   },
+      //   headStyles: { fillColor: [255, 255, 255], textColor: [0, 0, 0] },
+      //   tableLineWidth: 0,
+      // });
+
+      // const pdfData = doc.output("arraybuffer");
+      // zip.file(`${data.name}.pdf`, pdfData);
+      // promises.push(pdfData);
+    }
 
     // Promise.all(promises).then(() => {
     //   zip.generateAsync({ type: "blob" }).then((content) => {
